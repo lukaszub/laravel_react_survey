7881ca8eb82cfc60e8414455a3f236d17d87513e
lukaszub <44746812+lukaszub@users.noreply.github.com>
2023-04-13 18:18:10 +0200

    Creating Login/Signup/Dashboard/Surveys routes

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,19 +1,12 @@
 
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 
-export default function App() {
+export default function Signup() {
   return (
     <>
-      <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
+
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
+              Signup for free
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
@@ -21,7 +14,7 @@ export default function App() {
                 start your 14-day free trial
               </a>
             </p>
-          </div>
+
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
@@ -87,8 +80,6 @@ export default function App() {
               </button>
             </div>
           </form>
-        </div>
-      </div>
     </>
   )
 }
